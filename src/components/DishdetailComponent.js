a40cb96f6ad17009e078321d62a568f678bd602e
Lukas <biruta.lukas@gmail.com>
2020-10-13 14:54:50 +0300

    Presentational and Container Components

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class DishDetail extends Component {
+
+    renderDish() {
+        
+        const dish = this.props.selectedDish;
+
+        if (dish != null) {
+            return (
+                <Card key={dish.id}>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody> 
+                </Card>
+            );
+        }
+        else {
+            return (
+                <div></div>
+            );
+        }
+    }
+
+    renderComments(dish) {
+
+        if (dish != null) {
+
+            const comments = dish.comments.map((comment) => {
+
+                return (
+                    <ul key={comment.id} className="list-unstyled">
+                        <li>
+                            {comment.comment}
+                        </li>
+                        <li>
+                            -- {comment.author}, { }
+                            {
+                                new Intl.DateTimeFormat('en-US', {
+                                    year: 'numeric', month: 'short', day: '2-digit'
+                                }).format(new Date(Date.parse(comment.date)))
+                            }
+                        </li>
+                    </ul>
+                );
+            });
+
+            return (
+                <div className="p-3">
+                    <h4>Comments</h4>
+                    { comments }
+                </div>
+            );
+
+        } else {
+            return (
+                <div></div>
+            );
+        }
+    }
+    
+    render() {
+        return (
+            <div className="container">
+                <div className="col-12 col-md-5 m-1">
+                    { this.renderDish() }
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    {this.renderComments(this.props.selectedDish)}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default DishDetail;
